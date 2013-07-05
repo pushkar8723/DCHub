5e836dce017fbd8a6b86c52e96b201ccbe36ab44
pushkar8723 <pushkar8723@gmail.com>
2013-07-05 06:45:28 +0530

    search improved

--- files/search.php
+++ files/search.php
@@ -6,7 +6,12 @@ if (isset($_GET['page']) && $_GET['page'] > 0) {
 }
 if (isset($_GET['code'])) {
     $_GET = secure($_GET);
-    $body = "from dchub_content where title like '%$_GET[code]%'";
+    $select = array();
+    foreach(explode(' ', $_GET['code']) as $term){
+        array_push($select, "select * from dchub_content where deleted = 0 and title like '%$term%'");
+    }
+    $table = implode(" union ", $select);
+    $body = "from ($table)t order by timestamp";
     $res = DB::findAllWithCount("select *", $body, $page, 20);
     $data = $res['data'];
     ?>
@@ -30,6 +35,18 @@ if (isset($_GET['code'])) {
     <?php
     pagination($res['noofpages'], SITE_URL . "/search/" . $_GET['code'], $page, 10);
 } else {
-    
+    ?>
+    <script type='text/javascript'>
+        $(document).ready(function() {
+            $('form').submit(function() {
+                $(location).attr('href', '<?php echo SITE_URL; ?>/search/' + $('#search').val());
+                return false;
+            });
+        });
+    </script>
+    <form style='margin-top: 10px;' method='post' action='<?php echo SITE_URL; ?>/process.php'>
+        <input id='search' name='search' type='text' class='search-query' placeholder='Search'/>
+    </form>
+    <?php
 }
 ?>
